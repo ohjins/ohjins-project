98a0765965de0bb85daf15ffe264412216976701
ohjins <wlstjr1467@gmail.com>
2020-07-06 16:24:13 +0900

    프로젝트영역레이어팝업작업

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ function offsetMove(i){
   var offset = $(".set"+ i).offset().top;
   $('html, body').animate({scrollTop : offset}, 400); 
 }
+
+$(function(){
+  $(".layer_open").click(function(){
+    var windowTop = $(window).scrollTop();
+
+    $(this).closest('article').addClass('active')
+    $('.layerpop').css("top",windowTop+100);
+    
+    $(".layer_close").click(function(){
+      $(this).closest('article').removeClass("active");
+    });
+
+  });
+});
+
