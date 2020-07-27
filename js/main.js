379f2c3dcf1a2cc8cda2fd0e9aaae8f04a680f6e
ohjins <wlstjr1467@gmail.com>
2020-07-27 13:42:48 +0900

    20200727

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 
+//메뉴 이동 스크립트
 function offsetMove(i){
   var offset = $(".set"+ i).offset().top;
   $('html, body').animate({scrollTop : offset}, 400); 
 }
+//메뉴 글자 컬러 스크립트
+$(function(){
+  $(".menu > ul > li").click(function(){
+    $(this).addClass("active").siblings().removeClass("active");
+  });
+});
+
+
 
 $(function(){
   $(".mo_menu_open").click(function(){
@@ -12,11 +21,11 @@ $(function(){
   $(".layer_open").click(function(){
     var windowTop = $(window).scrollTop();
 
-    $(this).closest('article').addClass('active')
-    $('.layerpop').css("top",windowTop+100);
+    $(this).closest("article").addClass("active")
+    $(".layerpop").css("top",windowTop+100);
     
     $(".layer_close").click(function(){
-      $(this).closest('article').removeClass("active");
+      $(this).closest("article").removeClass("active");
     });
 
   });
